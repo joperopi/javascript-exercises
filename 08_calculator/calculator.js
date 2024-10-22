96a9f3fa053953829c408b51012c515643a2a738
joperopi <70579397+joperopi@users.noreply.github.com>
2024-10-22 16:57:13 -0300

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, current) => (total * current));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = a;
+  for (let i = 0; i < b -1; i++) {
+    result *= a;
+    console.log(result);
+  };
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let arr = []
+  if (num < 0) {
+    return -1;
+  } else if (num <= 1) {
+    return 1;
+  } else {
+    for (let i = num; i > 0; i--) {
+      arr.push(i);    
+    };
+    console.log(arr);
+    return arr.reduce((total, current) => (total * current));
+  };
 };
 
 // Do not edit below this line
