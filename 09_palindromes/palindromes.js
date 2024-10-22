c4c66ae3958004bdd481a8345a2a709106bdd701
joperopi <70579397+joperopi@users.noreply.github.com>
2024-10-22 18:21:14 -0300

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let norm = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let rev = norm.split("").reverse().join("");
+    if (norm === rev) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
