0d5d05a0ebc92c51e3e63ec582efc42f9ce9b83f
joperopi <70579397+joperopi@users.noreply.github.com>
2024-10-22 18:45:05 -0300

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    number = Number(num);
+    if (number < 0) {
+        return "OOPS"
+    } else if (number === 0) {
+        return 0;
+    } else if (number === 1) {
+        return 1;
+    };
+    let fibonacci = [0, 1, 1];
+    for (let i = 0; i < num; i++) {
+        fibonacci.push(fibonacci[fibonacci.length-1] + fibonacci[fibonacci.length-2]);
+    };
+    return fibonacci[num];
 
 };
 
