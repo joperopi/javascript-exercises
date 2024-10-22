9fca75f5323b2beeff67267d00a96c8d35ad9133
joperopi <70579397+joperopi@users.noreply.github.com>
2024-10-22 19:46:24 -0300

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let arrCopy = arr.map(x => x)
+    for (let i = 0; i < arrCopy.length; i++) {
+        if (arrCopy[i].yearOfDeath == undefined) {
+            arrCopy[i]["age"] = (new Date().getFullYear() - arrCopy[i].yearOfBirth);
+        } else {
+            arrCopy[i]["age"] = (arrCopy[i].yearOfDeath - arrCopy[i].yearOfBirth);
+        };
+    };
+    arrCopy.sort((a, b) => b.age - a.age);
+    return arrCopy[0];
 };
 
 // Do not edit below this line
